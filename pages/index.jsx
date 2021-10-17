fe9778bbb97211b1569be12f676b5e68f19757d3
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2021-10-17 14:54:01 +0200

    Adding next/head to page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,15 @@
+import Head from 'next/head'
 import Title from '../components/Title'
 
 export default function Home() {
   return (
   <>
+    <Head>
+        <title>Discord Timestamp Formatter</title>
+        <meta name="description" content="Little app that helps you create Timestamps for Discord." />
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+
     <Title>Hello?</Title>
     <p>Yo!</p>
   </>
