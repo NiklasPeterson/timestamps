53a6618b7ac98dee8a973531bc484301859eaf61
Niklas Peterson <5664205+NiklasPeterson@users.noreply.github.com>
2024-09-21 17:49:35 +0000

    Adds Discord button

--- pages/index.js
+++ pages/index.js
@@ -73,6 +73,10 @@ export default function Home() {
           <ScMain>
             <ScTitle>Timestamps Generator</ScTitle>
             <ScDescription>This little app helps you generate and format timestamps for Discord and other services that support timestamp format.</ScDescription>
+            <ScDiscordButton href="https://discord.com/oauth2/authorize?client_id=1031598414280015915" target='_blank'>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z"/></svg>
+              <span>Add Discord bot</span>
+            </ScDiscordButton>
             <ScSelectWrapper>
               <ScAction>Pick date</ScAction>
               <Datepicker newTime={handleDateTimeChange} />
@@ -134,11 +138,12 @@ const ScMain = styled.main`
   flex-direction: column;
   box-sizing: border-box;
   padding: 40px 24px;
+  align-items: center;
 `;
 
 const ScTitle = styled.h1`
-  font-size: 32px;
-  font-weight: 200;
+  font-size: 48px;
+  font-weight: 400;
   text-align: center;
   margin-bottom: 8px;
 `;
@@ -146,6 +151,36 @@ const ScTitle = styled.h1`
 const ScDescription = styled.p`
   text-align: center;
   margin-top: 0;
+`;
+
+const ScDiscordButton = styled.a`
+  display: flex;
+  height: 32px;
+  width: fit-content;
+  background: rgba(88, 101, 242, 1);
+  font-size: 14px;
+  font-weight: 500;
+  color: #fff;
+  padding: 4px 12px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  gap: 4px;
+  text-decoration: none;
+  margin-bottom: 16px;
+
+  &:hover {
+    background: rgba(88, 101, 242, 0.8)
+  }
+
+  span {
+    margin: 0 4px;
+  }
+
+  svg {
+    width: 24px;
+    height: 24px
+  }
 `;
 
 const ScAction = styled.p`
