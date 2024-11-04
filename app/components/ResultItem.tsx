c87af407758448eadafa218b9185a2f92ac63960
NiklasPeterson <5664205+NiklasPeterson@users.noreply.github.com>
2024-11-04 16:39:10 +0100

    Changing copy for the toast

--- app/components/ResultItem.tsx
+++ app/components/ResultItem.tsx
@@ -8,14 +8,20 @@ interface ResultItemProps {
   value: string;
 }
 
+const SuccessIcon: React.FC = ({ }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+  </svg>
+);
+
 const ResultItem: React.FC<ResultItemProps> = ({ children, label, value }) => {
   const handleButtonClick = () => {
     navigator.clipboard.writeText(value);
-    toast.success(`Copied to clipboard: ${value}`, {
-      className: 'backgroundSecondary contentPrimary borderSecondary',
-      // icon: <MyIcon />,
-    }
-    );
+
+    toast.success('Timestamp copied to your clipboard!', {
+      className: 'backgroundPrimary contentPrimary borderSecondary',
+      icon: <SuccessIcon />,
+    });
   };
 
   return (
